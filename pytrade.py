f432118dc05f51d6038fe9a69c54c3914f9d3513
codebummer <holysmokes@hanmail.net>
2022-01-28 18:10:25 +0900

    Code up to Day 2 of 파이썬으로 배우는 알고리즈 트레이딩

--- pytrade.py
+++ pytrade.py
@@ -20,6 +20,15 @@ class MyWindow(QMainWindow, form_class):
         self.timer.start(1000)
         self.timer.timeout.connect(self.timeout)
 
+        self.lineEdit.textChanged.connect(self.code_changed)
+
+        accouns_num = int(self.kiwoom.get_login_info("ACCOUNT_CNT"))
+        accounts = self.kiwoom.get_login_info("ACCNO")
+        accounts_list = accounts.split(';')[0:accouns_num]
+        self.comboBox.addItems(accounts_list)
+
+        self.pushButton.clicked.connect(self.send_order)
+
     def timeout(self):
         current_time = QTime.currentTime()
         text_time = current_time.toString("hh:mm:ss")
@@ -32,6 +41,24 @@ class MyWindow(QMainWindow, form_class):
             state_msg = "서버 미 연결 중"
 
         self.statusbar.showMessage(state_msg + " | " + time_msg)
+
+    def code_changed(self):
+        code = self.lineEdit.text()
+        name = self.kiwoom.get_master_code_name(code)
+        self.lineEdit_2.setText(name)
+
+    def send_order(self):
+        order_type_lookup = {'신규매수': 1, '신규매도': 2, '매수취소': 3, '매도취소': 4}
+        hoga_lookup = {'지정가': "00", '시장가': "03"}
+
+        account = self.comboBox.currentText()
+        order_type = self.comboBox_2.currentText()
+        code = self.lineEdit.text()
+        hoga = self.comboBox_3.currentText()
+        num = self.spinBox.value()
+        price = self.spinBox_2.value()
+
+        self.kiwoom.send_order("send_order_req", "0101", account, order_type_lookup[order_type], code, num, price, hoga_lookup[hoga], "")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
